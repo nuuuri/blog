57e311edfe73764064da85006a6a4720ef957ecd
nuuuri <sbfl125@gmail.com>
2024-08-05 23:13:42 +0900

    [feat] Route Groups 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false, // true인 경우 HTTP Status Code 308, false인 경우 307
+      },
+    ];
+  },
+};
 
 export default nextConfig;
